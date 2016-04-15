0f931e0d24cb9d689dc51ea6dd7b64af295da575
18101807476 <kent_wz@sina.cn>
2016-04-15 15:42:21 +0800

    Exe2.1.1

---
+++ Exe2.1.1/Exe2.1.1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exe2.1.1
+//
+//  Created by 王正 on 16/4/15.
+//  Copyright © 2016年 王正. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
