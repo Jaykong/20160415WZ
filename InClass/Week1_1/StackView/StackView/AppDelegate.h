e9fa3e2e1b9a18383c45ba7b47808e3842912d73
18101807476 <kent_wz@sina.cn>
2016-04-15 16:58:34 +0800

    StackView

---
+++ InClass/Week1_1/StackView/StackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by 王正 on 16/4/15.
+//  Copyright © 2016年 王正. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
